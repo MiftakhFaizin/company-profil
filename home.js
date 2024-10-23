60de5075699c28642bf942da37b9b2d03c0b14fb
MiftakhFaizin <miftakhdamar07@gmail.com>
2024-10-23 20:01:07 +0700

    home_page

---
+++ home.js
@@ -0,0 +1,35 @@
+let orderBtn = document.getElementById("order-btn")
+let orderDisplay = document.getElementById("order")
+let closeOrderBtn = document.getElementById("btn-close-order")
+
+orderBtn.addEventListener("click", () => {
+    orderDisplay.style.display = "flex"
+})
+
+closeOrderBtn.addEventListener("click", () => {
+    orderDisplay.style.display = "none"
+})
+
+let sliderBanner = document.getElementById("slider-banner")
+let bannerImage = document.getElementById("banner-image")
+let btnSliderPrevious = document.getElementById("btn-slider-previous")
+let btnSliderNext = document.getElementById("btn-slider-next")
+const bannerImages = [
+    "assets/2112.i301.031.S.m004.c13.UI and UX designers concepts isometric composition.jpg",
+    "assets/7065c1d722ba56108c9b5c5612014d71.jpg",
+    "assets/—Pngtree—web development concept flat design_5363510.png"
+]
+let currentImage = 0
+
+btnSliderNext.addEventListener("click", () => {
+    if(currentImage == bannerImages.length -1) currentImage = 0
+    else currentImage++
+    bannerImage.setAttribute("src", bannerImages[currentImage])
+})
+
+btnSliderPrevious.addEventListener("click", () => {
+    if(currentImage == 0) currentImage = bannerImages.length - 1
+    else currentImage--
+    bannerImage.setAttribute("src", bannerImages[currentImage])
+})
+
